1232bcbb4de4ec7d583f3c98ac2411c3a9d0c5cb
Ricardo Junior <rjcjunior@hotmail.com>
2018-03-31 23:17:02 +0000

    Exercicio 3 feito

--- tarefa1/views.py
+++ tarefa1/views.py
@@ -28,10 +28,8 @@ def guarda_lembrete(request):
         messages.success(request, 'Lembrete Salvo com sucesso!')
     else:
         messages.error(request, 'Ocorreu um erro ao salvar as mensagens')
-
-    # volta para página anterior
+    #volta para pagina anterior 
     return redirect(request.META.get('HTTP_REFERER'))
-
 
 def lembretes(request, username=None):
     if request.method == 'POST':
@@ -50,7 +48,33 @@ def somatorio(request):
     inicio = int(request.GET.get('inicio'))
     fim = int(request.GET.get('fim'))
     somatorio = 0
+
+    #Fazer somatorio'''
     for i in range(inicio, fim+1):
         somatorio+=i
-    return render(request, 'tarefa1/somatorio.html', {'inicio':inicio, "fim":fim, "somatorio":somatorio})
+        
+    #Contagem por sessao'''
+    if not request.session.has_key('contsessao'):
+        request.session['contsessao'] = 1
+    else:
+        request.session['contsessao'] += 1
+    cont1 = request.session['contsessao']
+    
 
+    resp = render(request, 'tarefa1/somatorio.html', {'inicio':inicio, "fim":fim, "somatorio":somatorio, "contsessao":cont1})    
+
+    #Contagem por Cookie'''
+    if request.COOKIES.get('contcookie'):
+        contcookie = int(request.COOKIES.get('contcookie'))
+        contcookie+=1
+        resp.set_cookie('contcookie', contcookie)
+
+    else:
+        resp.set_cookie('contcookie', 1)
+        contcookie = 1
+    if cont1 > contcookie:
+        resp = render(request, 'tarefa1/somatorio.html', {'inicio':inicio, "fim":fim, "somatorio":somatorio, "contsessao":cont1, "contserver":cont1})    
+    else:
+        resp = render(request, 'tarefa1/somatorio.html', {'inicio':inicio, "fim":fim, "somatorio":somatorio, "contsessao":cont1, "contserver":contcookie})
+
+    return resp
